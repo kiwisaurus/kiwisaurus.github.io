152f2ae4a497c05eea33b80063759ed2c14ed2fc
Terrence Liu <terrencetliu2@hotmail.com>
2020-10-18 00:17:06 -0500

    Update quiz.js
    readjusted weights and put in case for ties

--- quiz.js
+++ quiz.js
@@ -1,40 +1,43 @@
 (function() {
   var questions = [{
     question: "What kind of gameplay do you want?",
-    choices: ["Pressure", "Outmaneuver", "Balanced"]
+    choices: ["Pressure", "Outmaneuver", "Balanced", "No Opinion"]
   }, {
     question: "What do you want to be best at?",
-    choices: ["Damage", "Durability", "Mobility", "Utility"]
+    choices: ["Damage", "Durability", "Mobility", "Utility", "No Opinion"]
   }, {
     question: "What range are you comfortable at?",
-    choices: ["Close", "Medium", "Long"]
+    choices: ["Close", "Medium", "Long", "No Opinion"]
   }, {
     question: "Do you enjoy playing keepaway?",
-    choices: ["Yes, I do social distance", "No, I'm in a frat"]
+    choices: ["Yes, I do social distance", "No, I'm in a frat", "No Opinion"]
   }, {
     question: "Do you enjoy rushing down you opponent?",
-    choices: ["Yes, I'm running at you right now", "No, I like my personal space"]
+    choices: ["Yes, I'm running at you right now", "No, I like my personal space", "No Opinion"]
   }, {
       question: "Do you enjoy staying in control?",
-      choices: ["Yes, the less they play the more I play", "No, there is not a finite amount of game to go around"]
+      choices: ["Yes, the less they play the more I play", "No, there is not a finite amount of game to go around", "No Opinion"]
   }, {
       question: "Do you want stances?",
-      choices: ["Yes, what moves I have should be a mixup", "No, If I wanted more moves I'd play a tag fighter"]
+      choices: ["Yes, what moves I have should be a mixup", "No, If I wanted more moves I'd play a tag fighter", "No Opinion"]
   }, {
       question: "Do you want a projectile?",
-      choices: ["Yes, I want one that goes across the whole screen and ideally into someone else's game", "Yes, but as long as it controls space it's fine", "No, dashing is the best projectile anyway"]
+      choices: ["Yes, I want one that goes across the whole screen and ideally into someone else's game", "Yes, but as long as it controls space it's fine", "No, dashing is the best projectile anyway", "No Opinion"]
   }, {
       question: "Do you want setplay?",
-      choices: ["Yes, the key to winning is a flowchart to victory", "No, I'd rather focus on other things"]
+      choices: ["Yes, the key to winning is a flowchart to victory", "No, I'd rather focus on other things", "No Opinion"]
   }, {
       question: "Do you want a reversal?",
-      choices: ["Yes, meterless traditional Dragon Punch", "Yes, but metered or nontraditional is fine", "No, I can block" ]
+      choices: ["Yes, meterless traditional Dragon Punch", "Yes, but metered or nontraditional is fine", "No, I can block" , "No Opinion"]
+  }, {
+      question: "Do you want charge moves?",
+      choices: ["Yes, I want to charge", "No, I'm not into that", "No Opinion"]
   }, {
       question: "What tier do you want your character to be at?",
-      choices: ["High", "Mid", "Low"]
+      choices: ["High", "Mid", "Low", "No Opinion"]
   }, {
       question: "How difficult do you want your character to be?",
-      choices: ["Easy Difficulty", "Medium Difficulty", "Hard Difficulty"]
+      choices: ["Easy Difficulty", "Medium Difficulty", "Hard Difficulty", "No Opinion"]
   }];
   
   var questionCounter = 0; //Tracks question number
@@ -168,40 +171,56 @@
   }
   
   // Computes score and returns a paragraph element to be displayed
-   function displayScore() {
+  function displayScore() {
     var answerKey = {
         "Pressure":["ABA","Baiken","Eddie","Faust","I-no","Johnny","Justice","Kliff","May","Order Sol","Potemkin","Slayer","Venom","Zappa","Testament",1],
         "Outmaneuver":["Bridget","Chipp","Dizzy","Jam","Millia",1],
         "Balanced":["Anji","Axl","Ky","Sol","Robo Ky",1],
+        
         "Damage":["Baiken","I-no","Jam","Johnny","Kliff","May","Order Sol","Slayer","Sol","Venom",1],
         "Durability":["ABA","Anji","Justice","Potemkin",1],
         "Mobility":["Dizzy","Millia",1],
         "Utility":["Axl","Bridget","Chipp","Eddie","Faust","Ky Kiske","Robo Ky","Testament","Zappa",1],
+        
         "Close":["ABA","Sol","Jam","Order Sol","Slayer","Potemkin","Chipp","May",2],
         "Medium":["Ky","Anji","Millia","Baiken","Robo Ky","Johnny","Axl","Kliff","Bridget","Zappa","I-no",2],
         "Long":["Faust","Justice","Testament","Venom","Dizzy","Eddie",2],
+        
         "Yes, I do social distance":["Bridget","Dizzy","Justice","Venom","Testament","Axl","Faust",3],
         "No, I'm in a frat":["Bridget","Dizzy","Justice","Venom","Testament","Axl","Faust",-1],
+        
         "No, I like my personal space":["Sol","Order Sol","Slayer","Chipp","Millia","Potemkin","Kliff","Jam","ABA","May",-1],
         "Yes, I'm running at you right now":["Sol","Order Sol","Slayer","Chipp","Millia","Potemkin","Kliff","Jam","ABA","May",3],
+        
         "Yes, the less they play the more I play":["Baiken","Eddie","Dizzy","Anji","Chipp","I-no","Johnny","Justice","Kliff","Ky","Millia","Order Sol","Testament","Venom",3],
-        "No, there is not a finite amount of game to go around":["Baiken","Eddie","Dizzy","Anji","Chipp","I-no","Johnny","Justice","Kliff","Ky","Millia","Order Sol","Testament","Venom",-1],
-        "Yes, what moves I have should be a mixup":["Zappa","Robo Ky", "Anji","Baiken","ABA","Testament",3],
-        "No, If I wanted more moves I'd play a tag fighter":["Zappa","Robo Ky", "Anji","Baiken","ABA","Testament",-2],
-        "Yes, I want one that goes across the whole screen and ideally into someone else's game":["Axl","Bridget","Dizzy","Eddie","I-no","Justice","Ky","Robo-Ky","Testament","Venom","ABA",2],
+        "No, there is not a finite amount of game to go around":["Baiken","Eddie","Dizzy","Anji","Chipp","I-no","Johnny","Justice","Kliff","Ky","Millia","Order Sol","Testament","Venom",-2],
+        
+        "Yes, what moves I have should be a mixup":["Zappa","Robo Ky", "Anji","Baiken","ABA","Testament", "Johnny",3],
+        "No, If I wanted more moves I'd play a tag fighter":["Zappa","Robo Ky", "Anji","Baiken","ABA","Testament","Johnny",-5],
+        
+        "Yes, I want one that goes across the whole screen and ideally into someone else's game":["Axl","Bridget","Dizzy","Eddie","I-no","Justice","Ky","Robo-Ky","Testament","Venom","ABA",3],
         "Yes, but as long as it controls space it's fine":["Anji","Baiken","Chipp","Faust","Johnny","Kliff","May","Millia","Sol","Zappa",2],
         "No, dashing is the best projectile anyway":["Jam","Slayer","Potemkin","Order Sol",2],
-        "Yes, the key to winning is a flowchart to victory":["Anji","Baiken","Chipp","Dizzy","Eddie","I-no","Kliff","Ky","Millia","Testament","Venom",2],
-        "No, I'd rather focus on other things":["Anji","Baiken","Chipp","Dizzy","Eddie","I-no","Kliff","Ky","Millia","Testament","Venom",-1],
-        "Yes, meterless traditional Dragon Punch":["Sol","Chipp","Ky","Order Sol","Jam",3],
+        
+        "Yes, the key to winning is a flowchart to victory":["Anji","Baiken","Chipp","Dizzy","Eddie","I-no","Kliff","Ky","Millia","Testament","Venom",3],
+        "No, I'd rather focus on other things":["Anji","Baiken","Chipp","Dizzy","Eddie","I-no","Kliff","Ky","Millia","Testament","Venom",-2],
+        
+        "Yes, meterless traditional Dragon Punch":["Sol","Chipp","Ky","Order Sol","Jam",2],
         "Yes, but metered or nontraditional is fine":["Robo Ky","ABA","Axl","Baiken","Bridget","Dizzy","Johnny","Justice","May","Millia","Slayer","Testament","I-no",1],
-        "No, I can block":["Anji","Eddie","Faust","Kliff","Potemkin","Venom","Zappa",3],
+        "No, I can block":["Anji","Eddie","Faust","Kliff","Potemkin","Venom","Zappa",1],
+        
+        "Yes, I want to charge":["Anji","Axl","May","Potemkin",3],
+        "No, I'm not into that":["Anji","Axl","May","Potemkin",-5],
+        
         "High":["Testament","Baiken","Zappa","Millia","Chipp","Faust","Jam","Dizzy",3],
         "Mid":["Sol","May","Eddie","Potemkin","Axl","Justice","Anji","Johnny","Venom","I-no","Slayer","Order Sol",3],
         "Low":["Ky","Kliff","Bridget","Robo Ky","ABA",3],
+        
         "Easy Difficulty":["Sol","ABA","Jam","Ky","Anji","Millia","Axl","Kliff","Faust","Justice","Potemkin",5],
         "Medium Difficulty":["Order Sol","Slayer","Chipp","Baiken","Robo Ky","Bridget","Testament",5],
-        "Hard Difficulty":["May","Johnny","I-no","Eddie","Dizzy","Zappa",2]
+        "Hard Difficulty":["May","Johnny","I-no","Eddie","Dizzy","Zappa",4],
+        
+        "No Opinion": [0]
     };
     
     var characterKey = {
@@ -238,7 +257,7 @@
             var characterToScore = answerKey[selections[i]][j];
             var scoreSpot = answerKey[selections[i]].length-1
             characterKey[characterToScore] += answerKey[selections[i]][scoreSpot];
-            console.log(characterKey[characterToScore]);
+            console.log(characterKey[characterToScore]+ ' '+characterToScore);
         }
     }
     var choice = "";
@@ -249,6 +268,10 @@
             choiceScore = characterKey[key1];
             choice = key1;
             console.log(choice);
+        }
+        
+        else if (characterKey[key1] == choiceScore){
+            choice = choice + ' or ' + key1;
         }
     }  
     score.append('You got ' + choice);
